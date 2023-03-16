ef461a5e9b3dbbc93e5d6cb0cc91d0f7026aebf4
Carlos Navarro <c.leandro-navarro@outlook.com>
2023-03-16 17:24:30 -0600

    redistribucion de DB file, inicio de maquetado HTML

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <title>Tienda de camisetas</title>
+    </head>
+    <body>
+        <!-- CABECERA-->
+        <!--MENU-->
+        <!--BARRA LATERAL-->
+        <!--CONTENIDO CENTRAL--> 
+    </body>
+</html>
