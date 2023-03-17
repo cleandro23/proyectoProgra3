ed18b313572f288bf0ef6970f138b93d9c40cd7c
Carlos Navarro <c.leandro-navarro@outlook.com>
2023-03-16 19:38:06 -0600

    Creacion de encabezado, menu de categorias, barra lateral(con form)

--- index.php
+++ index.php
@@ -3,11 +3,58 @@
     <head>
         <meta charset="utf-8">
         <title>Tienda de camisetas</title>
+        <link rel="stylesheet" href="assets/css/styles.css"/>
     </head>
     <body>
         <!-- CABECERA-->
+        <header id="header">
+            <div id="logo">
+                <img src="assets/img/munichlion.png" alt="Logo tienda"/>
+                <a href="index.php">
+                    Tienda
+                </a>
+            </div>
+        </header>
         <!--MENU-->
+        <nav id="menu">
+            <ul>
+                <li>
+                    <a href="#">Inicio</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 1</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 2</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 3</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 4</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 5</a>
+                </li>
+            </ul>
+        </nav>
         <!--BARRA LATERAL-->
+        <div id="content">
+            <aside id="lateral">
+                <div id="login" class="block_aside">
+                    <form action="#" method="post">
+                        <label for="email">Email</label>
+                        <input type="email" name="email"/>
+                        <label for="password">Contraseña</label>
+                        <input type="password" name="password"/>
+                        <input type="submit" value="Enviar"/>
+                    </form>
+                    <a href="#">Mis pedidos</a>
+                    <a href="#">Gestionar pedidos</a>
+                    <a href="#">Gestionar categorias</a>
+                </div>
+            </aside>
+        </div>
         <!--CONTENIDO CENTRAL--> 
     </body>
 </html>
